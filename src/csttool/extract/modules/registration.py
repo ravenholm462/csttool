5743bc4fa55d7922d5fd3895b56b3605c91ed32f
ravenholm462 <nalo.alem@gmail.com>
2025-12-26 21:14:38 +0100

    implemented affine registration function

--- src/csttool/extract/modules/registration.py
+++ src/csttool/extract/modules/registration.py
@@ -31,9 +31,104 @@ def load_mni_template(contrast="T1"):
 # use dipy.viz.regtools.overlay_slices to show comparison before and after registration
 # https://docs.dipy.org/1.0.0/examples_built/affine_registration_3d.html
 
-def compute_affine_registration():
+def compute_affine_registration(
+    static_image,
+    static_affine,
+    moving_image,
+    moving_affine,
+    nbins=32,
+    sampling_prop=None,
+    num_iters=[10000, 1000, 100],
+    sigmas=[3.0, 1.0, 0.0],
+    factors=[4, 2, 1],
+    verbose=True
+):
 
-    return
+    from dipy.align.imaffine import (transform_centers_of_mass,
+                                    AffineMap,
+                                    MutualInformationMetric,
+                                    AffineRegistration)
+
+    from dipy.align.transforms import (TranslationTransform3D,
+                                   RigidTransform3D,
+                                   AffineTransform3D)
+
+    if verbose:
+        print(f"Computing affine registration...")
+        print(f"    Static (subject): {moving_image.shape}")
+        print(f"    Moving: {static_image.shape}")
+
+    if verbose:
+        print("    Aligning centers of mass...")
+
+    centers_of_mass = transform_centers_of_mass(
+        static_image,
+        static_affine,
+        moving_image,
+        moving_affine
+    )
+
+    if verbose:
+        print("    Center of mass alignment complete")
+        print(f"   Commencing affine registration...")
+        print(f"   Num of iters (coarse, medium, fine): {num_iters}")
+        print(f"   Sigmas: {sigmas}")
+        print(f"   Factors: {factors}")
+    
+    metric = MutualInformationMetric(nbins=nbins, sampling_proportion=sampling_prop)
+
+    affreg = AffineRegistration(metric=metric,
+                            level_iters=level_iters,
+                            sigmas=sigmas,
+                            factors=factors)
+
+    transform = TranslationTransform3D()
+    params0 = None
+    starting_affine = center_of_mass.affine
+
+    translation = affreg.optimize(
+        static_image,
+        moving_image,
+        transform,
+        params0,
+        static_affine,
+        moving_affine,
+        starting_affine
+    )
+
+    transformed = translation.transform(moving_image)
+
+    transform = RigidTransform3D()
+    params0 = None
+    starting_affine = translation.affine
+    rigid = affreg.optimize(
+        static_image, 
+        moving_image, 
+        transform, 
+        params0,
+        static_affine, 
+        moving_affine,
+        starting_affine=starting_affine
+    )
+
+    transformed = rigid.transform(moving_image)
+
+    transform = AffineTransform3D()
+    params0 = None
+    starting_affine = rigid.affine
+    affine = affreg.optimize(
+        static_image, 
+        moving_image, 
+        transform, 
+        params0,
+        static_affine, 
+        moving_affine,
+        starting_affine=starting_affine
+    )
+
+    transformed = affine.transform(moving_image)
+    
+    return transformed
 
 
 def compute_syn_registration():
